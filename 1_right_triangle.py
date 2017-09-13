f8b1abbf65ab29d211890a95ad2f8d00556d8191
Chirag Bohet <bohet.chirag@gmail.com>
2017-09-13 11:48:41 +0530

    Add files via upload
    Program 1
    Print a Right angle Triangle using asterisks.

---
+++ 1_right_triangle.py
@@ -0,0 +1,27 @@
+def rtTriangle():
+    
+    '''
+    objective : to draw a right angle triangle 
+    approach  : printing multiple lines of asterisks
+    '''
+    
+    print ('*')
+    print ('* *')
+    print ('* * *')
+    print ('* * * *')
+
+def main():
+    
+    '''
+    objective : to draw a right angle triangle
+    approach  : using function rtTriangle 
+    '''
+    
+    rtTriangle()
+    
+    print ('end of main')
+
+if __name__ == '__main__':
+    main()
+
+print ('end of program')

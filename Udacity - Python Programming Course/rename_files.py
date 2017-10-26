d5f1b8387ce084bcfc05056825d34230f9c91090
Chirag Bohet <bohet.chirag@gmail.com>
2017-10-27 01:05:48 +0530

    Function to rename files
    1) This function is used to Rename files in directory.
    2) It removes some specific characters from name of the file.

---
+++ Udacity - Python Programming Course/rename_files.py
@@ -0,0 +1,28 @@
+import os
+
+#Specify the address of the directory that you want to access
+directory = r"C:\Users\bohet\Documents\GitHub\Python\Udacity - Python Programming Course\prank"
+
+#create a translation table which is a parameter to .translate function, this table is used to remove specific characters from a string
+translation_table = str.maketrans("0123456789", "          ", "0123456789")
+
+def rename_files():
+    '''
+    Objective : To rename files in directory, removing specified characters from the name. In this case 0,1,2,3,4,5,6,7,8,9.
+    Input Variables : None
+    Output : The files would be renamed.
+    '''
+    os.chdir(directory)
+    
+    # (1) Get file names from a directory    
+    file_list = os.listdir(directory)
+    print(file_list)
+
+    # (2) Rename files
+    for file_name in file_list:
+        print("Old file Name - ", file_name)
+        print("New file Name - ", file_name.translate(translation_table))
+        os.rename(file_name,file_name.translate(translation_table))
+
+rename_files()
+    
